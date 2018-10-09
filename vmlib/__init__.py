8abeb7cd1cdee46e7d1bed2405ac72882481660f
Valentin Metraux <valentin.metraux@gmail.com>
2018-10-09 09:55:45 +0200

    Patch

--- vmlib/__init__.py
+++ vmlib/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Valentin Metraux"""
 __email__ = 'valentin@valentinmetraux.com'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 __all__ = ["dir", "hydrology", "io", "math", "pdf", "plot", "units",]
 
